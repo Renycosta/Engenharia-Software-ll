4ca9d10ffedce258090643306e00ab469374eaaa
Reny <reny.dev03@gmail.com>
2026-03-17 11:09:35 -0300

    Adiciona a function salvação

--- Main.js
+++ Main.js
@@ -7,3 +7,7 @@ console.log("Bem vindo ao GitHub!")
 // Modifica os arquivos do projeto e fazemos as alterações necessárias
 // git add: Adiciona os arquivos modificados para a área de staging
 // git commit: Salva as mudanças no repositório local
+
+function saudacao(){
+    console.log("Olá")
+}
