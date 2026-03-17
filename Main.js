ea14d7692bae8c4a5f09382b391656abdb38db6b
Reny <reny.dev03@gmail.com>
2026-03-17 09:44:21 -0300

    Adiciona comentário de fluxo

--- Main.js
+++ Main.js
@@ -1,3 +1,9 @@
 console.log("Olá, Git")
 
 console.log("Bem vindo ao GitHub!")
+
+// Fluxo de commit 
+
+// Modifica os arquivos do projeto e fazemos as alterações necessárias
+// git add: Adiciona os arquivos modificados para a área de staging
+// git commit: Salva as mudanças no repositório local
